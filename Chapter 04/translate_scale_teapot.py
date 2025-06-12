a36285eb441c4b0b3c415d76e9e67f601a6536ff
Eric Guo <eguo@yaocheng.cn>
2025-06-12 11:39:10 +0800

    fix

--- Chapter 04/translate_scale_teapot.py
+++ Chapter 04/translate_scale_teapot.py
@@ -3,18 +3,15 @@ from teapot import load_triangles
 from draw_model import draw_model
 
 def polygon_map(transformation, polygons):
-    return [
-        [transformation(vertex) for vertex in triangle]
-        for triangle in polygons
-    ]
+    return [[transformation(vertex) for vertex in triangle] for triangle in polygons]
 
 def scale2(v):
     return scale(2.0, v)
 
 def translate1left(v):
-    return add((-1,0,0), v)
+    return add((-1, 0, 0), v)
 
-def compose(f1,f2):
+def compose(f1, f2):
     def new_function(input):
         return f1(f2(input))
     return new_function
@@ -25,8 +22,9 @@ def compose(f1,f2):
 #### to run it, run this script with command line arg --snapshot
 import sys
 import camera
+
 if '--snapshot' in sys.argv:
-    camera.default_camera = camera.Camera('ex_translate_scale',[0])
+    camera.default_camera = camera.Camera('ex_translate_scale', [0])
 ####################################################################
 
 draw_model(polygon_map(compose(scale2, translate1left), load_triangles()))
