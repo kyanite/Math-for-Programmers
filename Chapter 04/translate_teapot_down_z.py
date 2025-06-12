6f0abef92390016b122df8ade6f24ed170a5b221
Eric Guo <eguo@yaocheng.cn>
2025-06-12 11:37:43 +0800

    fix

--- Chapter 04/translate_teapot_down_z.py
+++ Chapter 04/translate_teapot_down_z.py
@@ -3,13 +3,11 @@ from teapot import load_triangles
 from draw_model import draw_model
 
 def polygon_map(transformation, polygons):
-    return [
-        [transformation(vertex) for vertex in triangle] for triangle in polygons
-    ]
+    return [[transformation(vertex) for vertex in triangle] for triangle in polygons]
 
 def translate_by(translation):
     def new_function(v):
-        return add(translation,v)
+        return add(translation, v)
     return new_function
 
 ####################################################################
@@ -18,8 +16,9 @@ def translate_by(translation):
 #### to run it, run this script with command line arg --snapshot
 import sys
 import camera
+
 if '--snapshot' in sys.argv:
-    camera.default_camera = camera.Camera('ex_translate_teapot_down_z',[0])
+    camera.default_camera = camera.Camera('ex_translate_teapot_down_z', [0])
 ####################################################################
 
-draw_model(polygon_map(translate_by((0,0,-20)), load_triangles()))
+draw_model(polygon_map(translate_by((0, 0, -20)), load_triangles()))
